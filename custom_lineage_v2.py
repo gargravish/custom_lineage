0158791589d2db782c2a87a453093ef287af7638
RAVISH GARG <garg.ravish@gmail.com>
2024-10-30 11:08:07 +0000

    Update custom_lineage_v2.py

--- custom_lineage_v2.py
+++ custom_lineage_v2.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 import logging
-
 from google.cloud import datacatalog_lineage_v1 as lineage_v1
 from google.cloud.datacatalog_lineage_v1 import EventLink, LineageEvent, Process
 
@@ -17,16 +16,6 @@ def _convert_to_proto_timestamp(timestamp):
 
 
 def create_linage_process(project_id, process_display_name):
-    """
-    Creates a lineage process.
-
-    Args:
-        project_id (str): The ID of the project.
-        process_display_name (str): The display name of the process.
-
-    Returns:
-        str: The ID of the created process.
-    """
     parent = f"projects/{project_id}/locations/europe-west1"
     process = Process()
     process.display_name = process_display_name
@@ -46,24 +35,6 @@ def create_linage_process(project_id, process_display_name):
 
 
 def create_run(process_id, start_time, end_time, state, run_display_name):
-    """
-    Creates a lineage run.
-
-    Args:
-        process_id (str): The ID of the process.
-        start_time (str): The start time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        end_time (str): The end time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        state (str): The state of the run.
-        UNKNOWN = 0
-        STARTED = 1
-        COMPLETED = 2
-        FAILED = 3
-        ABORTED = 4
-        run_display_name (str): The display name of the run.
-
-    Returns:
-        str: The ID of the created run.
-    """
     run = lineage_v1.Run()
     run.start_time = start_time
     run.end_time = end_time
@@ -86,21 +57,6 @@ def create_run(process_id, start_time, end_time, state, run_display_name):
 
 
 def create_lineage_event(run_id, source_fqdn, target_fqdn, start_time, end_time):
-    """
-    Creates a lineage event.
-
-    Args:
-        run_id (str): The ID of the run.
-        source_fqdn (str): The fully qualified name of the source entity.
-        target_fqdn (str): The fully qualified name of the target entity.
-        start_time (str): The start time of the event (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        end_time (str): The end time of the event (format: %Y-%m-%dT%H:%M:%S.%fZ).
-
-    Returns:
-        None
-    """
-
-    #TODO: add a logic to check prefix (path:, coracle:)
     source = lineage_v1.EntityReference()
     target = lineage_v1.EntityReference()
     source.fully_qualified_name = source_fqdn
@@ -120,22 +76,6 @@ def create_lineage_event(run_id, source_fqdn, target_fqdn, start_time, end_time)
 
 def create_custom_linage_for_ingestion(project_id, process_display_name, source, target, start_time, end_time, state,
                                        run_display_name):
-    """
-    Creates a custom lineage for ingestion.
-
-    Args:
-        project_id (str): The ID of the project.
-        process_display_name (str): The display name of the process.
-        source (str): The fully qualified name of the source entity.
-        target (str): The fully qualified name of the target entity.
-        start_time (str): The start time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        end_time (str): The end time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        state (str): The state of the run.
-        run_display_name (str): The display name of the run.
-
-    Returns:
-        None
-    """
     process_id = create_linage_process(project_id, process_display_name=process_display_name)
     run_id = create_run(process_id=process_id, start_time=start_time, end_time=end_time, state=state,
                         run_display_name=run_display_name)
@@ -144,16 +84,6 @@ def create_custom_linage_for_ingestion(project_id, process_display_name, source,
 
 
 def _get_process_id(project_id, process_display_name):
-    """
-    Gets the ID of a process with the given display name.
-
-    Args:
-        project_id (str): The ID of the project.
-        process_display_name (str): The display name of the process.
-
-    Returns:
-        str: The ID of the process if found, None otherwise.
-    """
     parent = f"projects/{project_id}/locations/europe-west1"
     processes = client.list_processes(parent=parent)
     for process in processes:
@@ -164,26 +94,6 @@ def _get_process_id(project_id, process_display_name):
 
 def create_or_update_custom_linage_for_ingestion(project_id, process_display_name, source, target, start_time, end_time,
                                                  state, run_display_name):
-    """
-    Creates or updates a custom lineage for ingestion.
-
-    Args:
-        project_id (str): The ID of the project.
-        process_display_name (str): The display name of the process. - Airflow DAG Name
-        source (str): The fully qualified name of the source entity.
-        https://cloud.google.com//data-catalog/docs/fully-qualified-names
-
-        ## if path is provided it only shows file extension in linage UI (i.e. path:gs://raw-1234/test_schema/test.csv) else it shows file name (gs://raw-1234/test_schema/test.csv)
-
-        target (str): The fully qualified name of the target entity.
-        start_time (str): The start time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        end_time (str): The end time of the run (format: %Y-%m-%dT%H:%M:%S.%fZ).
-        state (str): The state of the run.
-        run_display_name (str): The display name of the run. - pas the task instance ID
-
-    Returns:
-        None
-    """
     process_name = _get_process_id(project_id, process_display_name)
     if process_name:
         logger.info("Process %s already exists", process_name)
@@ -201,21 +111,20 @@ def create_or_update_custom_linage_for_ingestion(project_id, process_display_nam
 if __name__ == '__main__':
     # Configure logger
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-    project_id = "raves-altostrat"
+    project_id = "project_id"
     process_display_name = "Dataplex_Custom_Lineage"
-    dataset_id = "data_journey"
-    table_id = "pubsub_direct"
-    subscription_id = "dj_subscription_bq_direct"
-    topic_name = "dj-pubsub-topic"
-    source = "projects/raves-altostrat/topics/dj-pubsub-topic"
-    target = "bigquery:raves-altostrat.data_journey.pubsub_direct"
+    dataset_id = "dataset_name"
+    table_id = "table_name"
+    subscription_id = "pubsub_subscription"
+    topic_name = "pubsub_topic"
+    source = f"projects/{project_id}/topics/{topic_name}"
+    target = f"bigquery:{project_id}.{dataset_id}.{topic_name}"
 
     start_time = datetime.now() - timedelta(hours=3)
     process_start_time = _convert_to_proto_timestamp(start_time)  # Start time dag
     process_end_time = _convert_to_proto_timestamp(datetime.now())  # End Time
 
     state = "COMPLETED"
-    run_display_name = "TASK_RUN_ID_2"
+    run_display_name = "PubSubtoBQv1"
     create_or_update_custom_linage_for_ingestion(project_id, process_display_name, source, target, process_start_time,
                                                  process_end_time, state, run_display_name)
